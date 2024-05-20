11921eddd780423cec22da7aaa7284b4d66a11af
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-05-20 12:06:55 +0200

    corrections

--- thesis/data/deployment/process.py
+++ thesis/data/deployment/process.py
@@ -15,6 +15,8 @@ BATTERY_VOLTAGE_MIN = 3.35 # V
 BATTERY_VOLTAGE_EMPTY = 2.8 # V
 BATTERY_CAPACITY = 0.300 # Ah
 BATTERY_CAPACITY_EMPTY = 0.030 # Ah
+CALIBRATION_LOW = [276,277,277,277]
+CALIBRATION_HIGH = [728,728,581,498]
 
 def average(data: pd.Series, window: int):
     ret = np.zeros_like(data)
@@ -27,10 +29,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('command', type=str, help='Command to execute', choices=['moisture', 'power'])
     parser.add_argument('--no-show', action='store_true', help='Do not show the plot')
+    parser.add_argument('--cal', action='store_true', help='Use the calibration coefficients to calculate relative values in moisture plot')
     args = parser.parse_args()
 
     OUT_BASE.mkdir(exist_ok=True)
-    name = args.command
+    name = args.command + ('-cal' if args.cal else '')
 
     colors = np.concatenate([
         plt.colormaps["Dark2"](np.linspace(0, 1, 8)),
@@ -47,20 +50,13 @@ def main():
         indexes.extend(np.where(data.index.diff() > pd.Timedelta(minutes=15))[0])
         indexes.append(len(data)-1)
         print(indexes)
-        value_max, value_min = 650, 250
-        
-        #for i in range(len(indexes) - 1):
-        #    d = data.iloc[indexes[i]:indexes[i+1]]
-        #    #d.plot(ax=ax, alpha=0.1, color=colors)
-        #    
-        #    #filtered = d.apply(average, window=4*45)
-        #    filtered = d.apply(average, window=4*30)
-        #    #filtered = filtered.apply(signal.savgol_filter, window_length=4*60, polyorder=2, mode='nearest')
-        #    #sos = signal.butter(2, 1/3600, 'low', fs=SAMPLING_RATE, output='sos')
-        #    #filtered = data.apply(lambda x: signal.sosfilt(sos, x))
-        #    #filtered = data.apply(average, window=4*15)
-        #    #filtered = filtered.iloc[200:]
-        #    #filtered.plot(ax=ax, color=colors)
+        value_max, value_min = (100, 0) if args.cal else (650, 250)
+        text_y = (value_max-value_min)*0.875 + value_min
+
+        if args.cal:
+            for i in range(4):
+                f = lambda x: (x - CALIBRATION_LOW[i]) / (CALIBRATION_HIGH[i] - CALIBRATION_LOW[i]) * 100
+                data[f'zone{i+1}'] = data[f'zone{i+1}'].apply(f)
 
         filtered = data.apply(average, window=int(SAMPLING_RATE*60*5))
         print(filtered.iloc[:5])
@@ -74,15 +70,15 @@ def main():
             d1 = data.iloc[indexes[i]:indexes[i+1]]
             d2 = data.iloc[indexes[i+1]:indexes[i+2]]
             ax.fill_betweenx([value_min,value_max], d1.index[-1], d2.index[0], color='0.95', hatch='/', edgecolor='0.8', linewidth=1)
-            ax.text(d1.index[-1] + pd.Timedelta(minutes=15), 600, 'No data', rotation=90, color='0.2')
+            ax.text(d1.index[-1] + pd.Timedelta(minutes=15), text_y, 'No data', rotation=90, color='0.2')
 
         watered = data[data.index == pd.to_datetime('24-05-12 10:10.03', format=TIME_FORMAT)].index
         ax.axvline(watered, color='0.2', linestyle='--', linewidth=1.5)
-        ax.text(watered + pd.Timedelta(minutes=15), 600, 'Watered', rotation=90, color='0.2')
+        ax.text(watered + pd.Timedelta(minutes=15), text_y, 'Watered', rotation=90, color='0.2')
         ax.get_legend().remove()
         ax.set_ylim(value_min, value_max)
         ax.grid(axis='y')
-        ax.set_ylabel('Rise time of the zone capacitor voltage [$\\mu$s]')
+        ax.set_ylabel('Relative soil moisture saturation [%]' if args.cal else 'Rise time of the zone capacitor voltage [$\\mu$s]')
         ax.set_xlabel('Time')
         ax.xaxis.set_major_formatter(DATE_FORMATTER)
 
